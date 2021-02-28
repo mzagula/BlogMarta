3463709ede018d304966999a2c05002a3005039c
Marta Zaguła <mzagula1992@gmail.com>
2021-02-28 20:59:56 +0100

    add markdown render to exmple blogpost

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+from flaskext.markdown import Markdown
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+markdown=Markdown(app)
+
 
 from app import routes
